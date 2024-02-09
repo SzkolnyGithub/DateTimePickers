e318a7fdc718d4d5044cfa363ffe4d6f9b6a62f5
GitMaster55 <144935449+SzkolnyGithub@users.noreply.github.com>
2024-02-09 13:56:06 +0100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Zadanie9._02Badowski4c;
+
+public partial class MainPage : ContentPage
+{
+	class Daty
+	{
+		public string Start { get; set; }
+		public string End { get; set; }
+		public string StartTime { get; set; }
+		public string EndTime { get; set; }
+	}
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void getJson(object sender, EventArgs e)
+	{
+		Daty data = new Daty
+		{
+			Start = dateStart.Date.ToShortDateString(),
+			End = dateEnd.Date.ToShortDateString(),
+			StartTime = timeStart.Time.ToString(),
+			EndTime = timeEnd.Time.ToString()
+        };
+		string dane = JsonConvert.SerializeObject(data, Formatting.Indented);
+        string path = Path.Combine(FileSystem.Current.AppDataDirectory, "data.json");
+        File.Delete(path);
+        File.WriteAllText(path, dane);
+		jsonTu.Text = File.ReadAllText(path);
+    }
+	
+}
